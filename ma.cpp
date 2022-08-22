f97c365ba0fde6d644986d3c20923d0f71277567
chabeerabsal <chabeerabsal@gmail.com>
2022-08-22 08:15:39 -0700

    matrix

---
+++ ma.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[3][2];
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<2;j++)
+		arr[i][j]=i+j;
+	}
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+	}
+}
