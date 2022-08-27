9ba83533a23bec0f4836ea9003bd48d757040fc8
chabeerabsal <chabeerabsal@gmail.com>
2022-08-27 08:27:15 -0700

    add

---
+++ gp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int a=84,b=87,c=3;
+	double r=((double)b)/a;
+	cout<<r<<endl;;
+	/*int ans=a*r^c-1;
+	cout<<ans;*/
+	//cout<<a*pow(r,c-1);
+	cout<<(double)a;
+	return 0;
+}
+
