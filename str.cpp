6721e0c69100f3aaa90e3714a5961f13c261d32e
chabeerabsal <chabeerabsal@gmail.com>
2022-09-02 09:56:24 -0700

    add

--- str.cpp
+++ str.cpp
@@ -1,20 +1,9 @@
-#include<iostrem>
+#include<iostream>
+#include<string.h>
 using namespace std;
 int main()
 {
-	string s="geeksforgeeks";
-	const int char=256;
-	boolean visitied[char]={0};
-	fill(visited,viside+char,false);
-	int res=-1;
-	for(int i=str.length()-1;i>=0;i--)
-	{
-		if(visited(str[i])
-		   str=i;
-		   else
-		   visited(str[i])=true;
-	}
-	return res
-	
-	}
+	string s="iam chabeer";
+	cout<<s.length();
+	return 0;
 }
